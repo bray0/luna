a04978dbe62055c1e58756753de1cf36463d6bb7
bray0 <briangichohis4@gmail.com>
2015-03-02 20:23:36 +0000

    improved Google History extaction
    Viewing of cookies, downloads made easier

--- History_viewer.py
+++ History_viewer.py
@@ -40,9 +40,9 @@ class Window(wx.Frame):
         self.SetTitle('History Viewer')
         #self.Center()
         self.SetPosition((300, 130))
-        self.SetSize((980, 580))
-        self.SetMaxSize((980, 580))
-        self.SetMinSize((980, 580))
+        self.SetSize((980, 598))
+        self.SetMaxSize((980, 598))
+        self.SetMinSize((980, 598))
         self.TreeUI()
         
         self.Bind(wx.EVT_MENU, self.OnQuit, id=105)
@@ -54,41 +54,25 @@ class Window(wx.Frame):
         self.panel1 = wx.Panel(self, -1, (0, 0), (0, 0), style=wx.SUNKEN_BORDER)
         #self.panel2 = wx.Panel(self, -1, (0, 0), (825, 348), style=wx.SUNKEN_BORDER)
         
-        self.panel2 = scrolled_panel = scrolled.ScrolledPanel(parent=self, id=-1, style=wx.SUNKEN_BORDER)
-        self.panel2.SetPosition((152, 2))
-        self.panel2.SetSize((825, 552))
-        scrolled_panel.SetBackgroundColour(wx.BLACK)
-        scrolled_panel.SetupScrolling()
         
         
         self.tree = wx.TreeCtrl(self.panel1, 1, wx.DefaultPosition, (-1, -1), wx.TR_HIDE_ROOT|wx.TR_HAS_BUTTONS)
         root = self.tree.AddRoot('Browsers')
         firefox = self.tree.AppendItem(root, 'Firefox')
         chrome = self.tree.AppendItem(root, 'Google Chrome')
-        self.tree.AppendItem(firefox, 'Url History')
-        self.tree.AppendItem(firefox, 'Cookies')
-        self.tree.AppendItem(firefox, 'Downloads')
-        self.tree.AppendItem(firefox, 'Popular Sites')
-        self.tree.AppendItem(firefox, 'Least Popular')
+        self.tree.AppendItem(firefox, 'F History')
+        self.tree.AppendItem(firefox, 'F Cookies')
+        self.tree.AppendItem(firefox, 'F Downloads')
+        self.tree.AppendItem(firefox, 'F High Hits')
+        self.tree.AppendItem(firefox, 'F Low Hits')
         
-        self.tree.AppendItem(chrome, 'Url History')
-        self.tree.AppendItem(chrome, 'Cookies')
-        self.tree.AppendItem(chrome, 'Downloads')
-        self.tree.AppendItem(chrome, 'Popular Sites')
-        self.tree.AppendItem(chrome, 'Least popular')
+        self.tree.AppendItem(chrome, 'G History')
+        self.tree.AppendItem(chrome, 'G Cookies')
+        self.tree.AppendItem(chrome, 'G Downloads')
+        self.tree.AppendItem(chrome, 'G High Hits')
+        self.tree.AppendItem(chrome, 'G Low Hits')
         
         
-        #============================================================
-        #                 LISTCtrl
-        #============================================================
-        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
-        
-        self.list = AutoWidthListCtrl(self.panel2)
-        self.list.InsertColumn(0, 'Hits', width=50)
-        self.list.InsertColumn(1, 'Last Visited', width=180)
-        self.list.InsertColumn(2, 'Url', width=140)
-        
-        hbox2.Add(self.list, 1, wx.EXPAND|wx.ALL)
         #============================================================
         #============================================================
         #                 STATUSBAR
@@ -100,16 +84,102 @@ class Window(wx.Frame):
         #============================================================
         self.Bind(wx.EVT_MENU, self.onBrowse, id=101)
         self.tree.Bind(wx.EVT_TREE_SEL_CHANGED, self.OnSelChanged, id=1)
+        
         #============================================================
         vbox.Add(self.tree, 1)
         hbox.Add(self.panel1, 1, wx.EXPAND)
         #hbox.Add(self.panel2, 1, wx.EXPAND)
         self.panel1.SetSizer(vbox)
-        self.panel2.SetSizer(hbox2)
         self.SetSizer(hbox)
     
+    def selfListCtrl(self):
+        self.panel2 = scrolled_panel = scrolled.ScrolledPanel(parent=self, id=-1, style=wx.SUNKEN_BORDER)
+        self.panel2.SetPosition((152, 2))
+        self.panel2.SetSize((825, 552))
+        scrolled_panel.SetBackgroundColour(wx.WHITE)
+        scrolled_panel.SetupScrolling()
+        
+        #============================================================
+        #                 LISTCtrl
+        #============================================================
+        self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.list = AutoWidthListCtrl(self.panel2)
+        self.list.InsertColumn(0, 'Hits', width=50)
+        self.list.InsertColumn(1, 'Last Visited', width=180)
+        self.list.InsertColumn(2, 'Url', width=140)
+        
+        self.list.Bind(wx.EVT_LIST_ITEM_SELECTED, self.selectedItem)
+        
+        self.hbox2.Add(self.list, 1, wx.EXPAND|wx.ALL)
+        
+        self.panel2.SetSizer(self.hbox2)
+    
+    def selfListCtrl2(self):
+        self.panel2 = scrolled_panel = scrolled.ScrolledPanel(parent=self, id=-1, style=wx.SUNKEN_BORDER)
+        self.panel2.SetPosition((152, 2))
+        self.panel2.SetSize((825, 552))
+        scrolled_panel.SetBackgroundColour(wx.WHITE)
+        scrolled_panel.SetupScrolling()
+        
+        #============================================================
+        #                 LISTCtrl
+        #============================================================
+        self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        ##################################
+        #     Creating new columns
+        ##################################
+        self.list = AutoWidthListCtrl(self.panel2)
+        self.list.InsertColumn(0, 'Start Time', width=160)
+        self.list.InsertColumn(1, 'Recived Bytes', width=120)
+        self.list.InsertColumn(2, 'Total Bytes', width=120)
+        self.list.InsertColumn(3, 'Target Path', width=200)
+        self.list.InsertColumn(4, 'Source URL', width=200 )
+                
+        self.list.Bind(wx.EVT_LIST_ITEM_SELECTED, self.selectedItemDown)
+        
+        self.hbox2.Add(self.list, 1, wx.EXPAND|wx.ALL)
+        
+        self.panel2.SetSizer(self.hbox2)
+    
+    def selfListCookieCtrl(self):
+        self.panel2 = scrolled_panel = scrolled.ScrolledPanel(parent=self, id=-1, style=wx.SUNKEN_BORDER)
+        self.panel2.SetPosition((152, 2))
+        self.panel2.SetSize((825, 552))
+        scrolled_panel.SetBackgroundColour(wx.WHITE)
+        scrolled_panel.SetupScrolling()
+        
+        #============================================================
+        #                 LISTCtrl
+        #============================================================
+        self.hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        ##################################
+        #     Creating new columns
+        ##################################
+        self.list = AutoWidthListCtrl(self.panel2)
+        self.list.InsertColumn(0, 'Creation Date', width=160)
+        self.list.InsertColumn(1, 'Url Host', width=180)
+        self.list.InsertColumn(2, 'Expiry Date', width=160)
+        self.list.InsertColumn(3, 'Is Secure?', width=80)
+        self.list.InsertColumn(4, 'Is HttpOnly?', width=80 )
+        self.list.InsertColumn(5, 'Last Access', width=160 )
+        
+        self.hbox2.Add(self.list, 1, wx.EXPAND|wx.ALL)
+        
+        self.panel2.SetSizer(self.hbox2)
+        
+    def selectedItemDown(self, event):
+        currentItem = event.m_itemIndex  
+        rowItem = self.myRowDict[currentItem]
+        self.StatusBar.SetStatusText('LOCATION PATH: => '+ rowItem[3])
+    
+    def selectedItem(self, event):
+        currentItem = event.m_itemIndex  
+        rowItem = self.myRowDict[currentItem]
+        self.StatusBar.SetStatusText(rowItem[2])
+        
     def onBrowse(self, event):
-        wildcard = "database (*.sqlite)|*.sqlite"
+        wildcard = "database (*.sqlite, *.*)|*.sqlite;*.*"
         dialog = wx.FileDialog(self, "Choose a File", defaultDir=os.getcwd(), defaultFile="*.sqlite", wildcard=wildcard, style=wx.OPEN)
         
         if dialog.ShowModal() == wx.ID_OK:
@@ -124,29 +194,150 @@ class Window(wx.Frame):
         textItem = self.tree.GetItemText(item)
         
         if self.browseStatus:
-            if textItem == "Url History":
+            if textItem == "F History":
                 self.list.DeleteAllItems()
-                data = self.databaseHistory(self.filePath)
-                self.fillInData(self.filePath, data)
-            if textItem == "Cookies":
+                filename = self.filePath.split('/')[-1]
+                if filename == "places.sqlite":
+                    data = self.databaseHistory(self.filePath)
+                    self.fillInData(self.filePath, data)
+            if textItem == "F Cookies":
                 self.list.DeleteAllItems()
-            if textItem == "Downloads":
+            if textItem == "F Downloads":
                 self.list.DeleteAllItems()
-            if textItem == "Popular Sites":
+            if textItem == "F High Hits":
+                filename = self.filePath.split('/')[-1]
+                if filename == "places.sqlite":
+                    data = self.databasePopular(self.filePath)
+                    self.fillInData(self.filePath, data)
+            if textItem == "F Low Hits":
                 self.list.DeleteAllItems()
-                data = self.databasePopular(self.filePath)
-                self.fillInData(self.filePath, data)
-            if textItem == "Least Popular":
-                self.list.DeleteAllItems()
-                data = self.databaseLessPopular(self.filePath)
-                self.fillInData(self.filePath, data)
+                filename = self.filePath.split('/')[-1]
+                if filename == "places.sqlite":
+                    data = self.databaseLessPopular(self.filePath)
+                    self.fillInData(self.filePath, data)
+            if textItem == "G History":
+                self.selfListCtrl()
+                
+                filename = self.filePath.split('/')[-1]
+                if filename == 'history':
+                    data = self.googleHistory(self.filePath)
+                    self.fillInData(self.filePath, data)
+                    
+            if textItem == "G High Hits":
+                self.selfListCtrl()
+                filename = self.filePath.split('/')[-1]
+                if filename == 'history':
+                    data = self.googleHighHits(self.filePath)
+                    self.fillInData(self.filePath, data)
+            if textItem == "G Low Hits":
+                self.selfListCtrl()
+                filename = self.filePath.split('/')[-1]
+                if filename == 'history':
+                    data = self.googleLowHits(self.filePath)
+                    self.fillInData(self.filePath, data)
+            if textItem == "G Downloads":
+                self.selfListCtrl2()
+                filename = self.filePath.split('/')[-1]
+                if filename == 'history':
+                    data = self.googleDownloads(self.filePath)
+                    self.fillInDownloads(self.filePath, data)
+            if textItem == "G Cookies":
+                self.selfListCookieCtrl()
+                filename = self.filePath.split('/')[-1]
+                if filename == 'Cookies':
+                    data = self.googleCookies(self.filePath)
+                    self.fillinCookies(self.filePath, data)
     
     def fillInData(self, filepath, data):
+        self.myRowDict = {}
         for i in data:
             index = self.list.InsertStringItem(sys.maxint, str(i[0]))
             self.list.SetStringItem(index, 1, i[1].decode('utf8', 'ignore'))
-            self.list.SetStringItem(index, 2, i[2].decode('utf8', 'ignore'))  
+            self.list.SetStringItem(index, 2, i[2].decode('utf8', 'ignore'))
+            self.myRowDict[index] = i  
+    def bytes2Human(self, digits):
+        answer = None
+        if str(digits).isdigit():
+            if digits >= 1024:
+                answer = "%.1fKB" % (digits / float(1024))
+            if digits >= 1048576:
+                answer = "%.1fMB" % (digits / float(1048576))
+            if digits >= 1073741824:
+                answer = "%.1fGB" % (digits / float(1073741824))
+
+        return str(answer)
+    
+    def boolean2Human(self, state):
+        answer = None
+        if state == 1:
+            answer = 'Yes'
+        else:
+            answer = 'No'
+        
+        return str(answer)
+    
+    def fillinCookies(self, filepath, data):
+        self.myRowDict = {}
+        
+        for i in data:
+            index = self.list.InsertStringItem(sys.maxint, str(i[0])) #creation date
+            self.list.SetStringItem(index, 1, i[1].decode('utf8', 'ignore')) #url host
+            self.list.SetStringItem(index, 2, str(i[2])) #expiry date
+            self.list.SetStringItem(index, 3, label=self.boolean2Human(i[3])) #is secure
+            self.list.SetStringItem(index, 4, label=self.boolean2Human([4])) #is httponly
+            self.list.SetStringItem(index, 5, str(i[5])) #last access
+            self.myRowDict[index] = i 
             
+    def fillInDownloads(self, filepath, data):
+        self.myRowDict = {}
+        
+        for i in data:
+            index = self.list.InsertStringItem(sys.maxint, str(i[0]))
+            self.list.SetStringItem(index, 1, label=self.bytes2Human(i[1]))
+            self.list.SetStringItem(index, 2, label=self.bytes2Human(i[2]))
+            self.list.SetStringItem(index, 3, i[3].decode('utf8', 'ignore'))
+            self.list.SetStringItem(index, 4, i[4].decode('utf8', 'ignore'))
+            self.myRowDict[index] = i 
+            
+    def googleLowHits(self, filename):
+        con = lite.connect(filename)
+        cur = con.cursor()
+        statement = 'SELECT urls.visit_count, datetime(((visits.visit_time/1000000)-11644473600), "unixepoch"), urls.url FROM urls, visits WHERE urls.id = visits.url ORDER BY urls.visit_count ASC'
+        cur.execute(statement)
+        row = cur.fetchall()
+        return row 
+    
+    def googleHighHits(self, filename):
+        con = lite.connect(filename)
+        cur = con.cursor()
+        statement = 'SELECT urls.visit_count, datetime(((visits.visit_time/1000000)-11644473600), "unixepoch"), urls.url FROM urls, visits WHERE urls.id = visits.url ORDER BY urls.visit_count DESC'
+        cur.execute(statement)
+        row = cur.fetchall()
+        return row 
+    
+    def googleCookies(self, filename):
+        con = lite.connect(filename)
+        cur = con.cursor()
+        statement = 'SELECT datetime(((cookies.creation_utc/1000000)-11644473600), "unixepoch"), cookies.host_key, datetime(((cookies.expires_utc/1000000)-11644473600), "unixepoch"), cookies.secure, cookies.httponly, datetime(((cookies.last_access_utc/1000000)-11644473600), "unixepoch") FROM cookies'
+        cur.execute(statement)
+        row = cur.fetchall()
+        return row
+    
+    def googleDownloads(self, filename):
+        con = lite.connect(filename)
+        cur = con.cursor()
+        statement = 'SELECT datetime(((downloads.start_time/1000000)-11644473600), "unixepoch"), downloads.received_bytes, downloads.total_bytes, downloads.target_path, downloads_url_chains.url FROM downloads, downloads_url_chains WHERE downloads.id = downloads_url_chains.id'
+        cur.execute(statement)
+        row = cur.fetchall()
+        return row 
+    
+    def googleHistory(self, filename):
+        con = lite.connect(filename)
+        cur = con.cursor()
+        statement = 'SELECT urls.visit_count, datetime(((visits.visit_time/1000000)-11644473600), "unixepoch"), urls.url FROM urls, visits WHERE urls.id = visits.url'
+        cur.execute(statement)
+        row = cur.fetchall()
+        return row 
     
     def databaseLessPopular(self, filename):
         con = lite.connect(filename)
